9291d7682d81903e98e47c28007b40d0e8907697
Ready2Learn2025 <dt1051669@gmail.com>
2025-07-14 01:11:51 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // Basic Firebase config placeholder
 // Replace the following with your own Firebase project settings
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyDsij-KlwU4ptuQZCc8JHh8h1I8dTA6VSk",
+  authDomain: "sereneai-c7769.firebaseapp.com",
+  projectId: "sereneai-c7769",
 };
 
 // Initialize Firebase
